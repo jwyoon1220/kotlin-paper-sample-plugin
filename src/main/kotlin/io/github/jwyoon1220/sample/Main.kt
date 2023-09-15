d5091e839712b9b38c66d77e71e5e40e46ce780e
jwyoon1220 <106739205+jwyoon1220@users.noreply.github.com>
2023-09-15 19:00:31 +0900

    Update Main.kt

--- src/main/kotlin/io/github/jwyoon1220/sample/Main.kt
+++ src/main/kotlin/io/github/jwyoon1220/sample/Main.kt
@@ -6,7 +6,7 @@ class Main : JavaPlugin() {
 
     override fun onEnable() {
         logger.warning("지금은 테스트 버전입니다. 오류가 있을 수 있습니다.")
-        logger.warning("오류가 발생한다면 깃버브 \"jwyoon1220\" 에다 아무 레포지트리 에서 이슈로 작성해주세요")
+        logger.warning("오류가 발생한다면 깃허브 \"jwyoon1220\" 에다 아무 레포지트리 에서 이슈로 작성해주세요")
         logger.info("활성화 됨")
         getCommand("test")?.setExecutor(Command())
     }
